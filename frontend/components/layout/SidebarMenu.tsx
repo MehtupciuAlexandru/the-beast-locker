4a4c4d3ec99cae7bcc3131803035643886707849
Alexandru Mehtupciu <523554@student.fontys.nl>
2026-03-23 15:00:18 +0200

    Update SidebarMenu.tsx
    Buttons route to products page. Temporary for stakeholder presentation

--- frontend/components/layout/SidebarMenu.tsx
+++ frontend/components/layout/SidebarMenu.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 
 type SidebarMenuProps = {
     isOpen: boolean;
@@ -15,7 +16,7 @@ export default function SidebarMenu({ isOpen, onClose }: SidebarMenuProps) {
                     isOpen ? "opacity-100 visible" : "opacity-0 invisible"
                 }`}
             />
-
+            
             {/* Sidebar */}
             <div
                 className={`fixed top-0 left-0 h-full w-[280px] bg-white z-50 transform transition-transform duration-300 ${
@@ -23,6 +24,7 @@ export default function SidebarMenu({ isOpen, onClose }: SidebarMenuProps) {
                 }`}
             >
                 <div className="p-4">
+
                     <button
                         onClick={onClose}
                         className="text-sm font-semibold text-black"
@@ -31,10 +33,39 @@ export default function SidebarMenu({ isOpen, onClose }: SidebarMenuProps) {
                     </button>
 
                     <div className="mt-6 flex flex-col gap-4 text-m font-semibold text-black">
-                        <button>Tricouri</button>
-                        <button>Hanorace</button>
-                        <button>Pantaloni</button>
-                        <button>Accesorii</button>
+
+                        <Link
+                            href="/products"
+                            onClick={onClose}
+                            className="block"
+                        >
+                            Tricouri
+                        </Link>
+
+                        <Link
+                            href="/products"
+                            onClick={onClose}
+                            className="block"
+                        >
+                            Hanorace
+                        </Link>
+
+                        <Link
+                            href="/products"
+                            onClick={onClose}
+                            className="block"
+                        >
+                            Pantaloni
+                        </Link>
+
+                        <Link
+                            href="/products"
+                            onClick={onClose}
+                            className="block"
+                        >
+                            Accesorii
+                        </Link>
+
                     </div>
                 </div>
             </div>
