e8afd8a6ec4c82c1e0dbac1aaa6d740d22c11ec9
Alexandru Mehtupciu <523554@student.fontys.nl>
2026-04-02 01:46:00 +0300

    Update auth-validation-plugin.ts

--- src/plugins/auth-validation/auth-validation-plugin.ts
+++ src/plugins/auth-validation/auth-validation-plugin.ts
@@ -1,9 +1,15 @@
 import { VendurePlugin, PluginCommonModule } from '@vendure/core';
 import{AuthValidationResolver} from "./api/auth-validation-resolver";
+import gql from 'graphql-tag';
 
 @VendurePlugin({
     imports: [PluginCommonModule],
     shopApiExtensions: {
+        schema: gql`
+            extend type Mutation {
+                validatedRegisterCustomerAccount(input: RegisterCustomerInput!): RegisterCustomerAccountResult!
+            }
+        `,
         resolvers: [AuthValidationResolver],
     },
 })
