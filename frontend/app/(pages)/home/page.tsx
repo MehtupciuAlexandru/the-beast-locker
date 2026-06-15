3be97016510ec4cdac863377529665e1ed3f565a
Alexandru Mehtupciu <523554@student.fontys.nl>
2026-06-15 11:19:51 +0300

    Update page.tsx

--- frontend/app/(pages)/home/page.tsx
+++ frontend/app/(pages)/home/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
             <Hero />
             <PlatformDetails />
             <Categories />
-            <FeaturedProducts products={mockProducts} />
+            <FeaturedProducts />
             <BannerSection />
             <Story />
             <Newsletter />
